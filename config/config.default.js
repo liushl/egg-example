50c29a9117e295187203121ece64ad5992dd0db8
liushl <shengliniam@163.com>
2020-11-23 18:55:34 +0800

    commit

--- config/config.default.js
+++ config/config.default.js
@@ -10,7 +10,28 @@ module.exports = appInfo => {
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = exports = {
+    // 数据库配置
+    mysql: {
+      // 单数据库信息配置
+      client: {
+        // host
+        host: 'localhost',
+        // 端口号
+        port: '3306',
+        // 用户名
+        user: 'root',
+        // 密码
+        password: 'admin1234',
+        // 数据库名
+        database: 'schema_ops',
+      },
+      // 是否加载到 app 上，默认开启
+      app: true,
+      // 是否加载到 agent 上，默认关闭
+      agent: false,
+    },
+  };
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_112233';
